52123cd5d2b41aca9b0fb636f01ace9c35216c55
mystique4u <mystique4u@gmail.com>
2023-07-23 14:27:37 -0400

    Final

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -3,10 +3,10 @@ from deep_translator import MyMemoryTranslator
 
 def english_to_french(english_text):
     """Translation from English to French"""
-    french_text = MyMemoryTranslator(source='en-GB', target='fr-FR').translate(english_text)
+    french_text = MyMemoryTranslator(source='en', target='fr').translate(english_text)
     return french_text
 
 def french_to_english(french_text):
     """French to English translation"""
-    english_text = MyMemoryTranslator(source='fr-FR', target='en-GB').translate(french_text)
+    english_text = MyMemoryTranslator(source='fr', target='en').translate(french_text)
     return english_text
