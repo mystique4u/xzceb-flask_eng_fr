03348d476a4c3849859ea194ce8b3af6db37f285
mystique4u <mystique4u@gmail.com>
2023-07-23 14:04:25 -0400

    I hope it is finished

--- final_project/server.py
+++ final_project/server.py
@@ -21,8 +21,6 @@ def frenchToEnglish():
 @app.route("/")
 def renderIndexPage():
    return render_template('index.html')
-if __name__ == '__main__':
-   app.run()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080, debug=True)
